61e8722f75d5493fbcb37131b53cd9ee6be363f3
Alexsandro Machado <lekomachado@hotmail.com>
2021-02-28 09:54:10 -0300

    Created Author DTO class

---
+++ src/main/java/com/alex/bookstoremanager/author/dto/AuthorDTO.java
@@ -0,0 +1,30 @@
+package com.alex.bookstoremanager.author.dto;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorDTO {
+	
+	    private Long id;
+
+	   @NotNull
+	   @NotEmpty
+	   @Size(max = 255)
+	    private String name;
+
+	 @NotNull
+	 @Max(120)
+	    private int age;
+
+}
