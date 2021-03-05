b158d9c49b559e6da7731e4bf3b74a4c56cebe02
Alexsandro Machado <lekomachado@hotmail.com>
2021-03-05 09:27:11 -0300

    Created jwt request and response classes

---
+++ src/main/java/com/alex/bookstoremanager/users/dto/JwtRequest.java
@@ -0,0 +1,23 @@
+package com.alex.bookstoremanager.users.dto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtRequest {
+	
+	@NotNull
+	@NotEmpty
+	private String username;
+
+	@NotNull
+	@NotEmpty
+	private String password;
+
+}
